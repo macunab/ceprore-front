13b158ec8ad7b0ba8c9d01904491972b56c6f7d2
Marshall <mn.acunab@gmail.com>
2024-03-21 18:08:43 -0300

    env set

--- docs/chunk-4ZULC53F.js
+++
@@ -1,1 +0,0 @@
-import{Lb as o,Mb as i,qb as e,u as t}from"./chunk-4YGZJTM3.js";var h=(a,n)=>{let r=t(i),u=t(e);return r.tokenValidation().subscribe({}),r.authStatus()===o.authenticated?!0:(u.navigateByUrl("login"),!1)};export{h as a};
---
+++ docs/chunk-XDDWQCRH.js
@@ -0,0 +1,1 @@
+import{Lb as o,Mb as i,qb as e,u as t}from"./chunk-YHPWZFCP.js";var h=(a,n)=>{let r=t(i),u=t(e);return r.tokenValidation().subscribe({}),r.authStatus()===o.authenticated?!0:(u.navigateByUrl("login"),!1)};export{h as a};
