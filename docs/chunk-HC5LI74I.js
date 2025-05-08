c607e57a1d941abf5f595a060061d1b3879e7db8
Marshall <mn.acunab@gmail.com>
2025-05-08 00:45:50 -0300

    remove docs

--- docs/chunk-HC5LI74I.js
+++
@@ -1,1 +0,0 @@
-import{Lb as e,Mb as o,qb as r,u as t}from"./chunk-YHPWZFCP.js";var f=(i,m)=>{let u=t(o),a=t(r);return u.authStatus()===e.authenticated?(a.navigateByUrl("main/home"),!1):!0};export{f as a};
